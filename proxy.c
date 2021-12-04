66d833f39dc6df327f2f8ef5bca88b1c65365c97
ljuneh <glglglr670@gmail.com>
2021-12-04 14:09:34 +0900

    2021.12.04

--- proxy.c
+++ proxy.c
@@ -31,7 +31,7 @@ int main(int argc, char **argv){
     struct sockaddr_in clientaddr;                                      //client 의 socket address
     pthread_t tid;
 
-    //Signal(SIGPIPE, sigpipe_handler);                                 /**********/
+    //Signal(SIGPIPE, sigpipe_handler);      
     pthread_mutex_init(&mutex_lock, NULL);                              //mutex 초기화
 
     int portnum = atoi(argv[1]); 
@@ -52,7 +52,7 @@ void *thread(void *vargp){
     struct sockaddr_in sockaddr = connec->addr;
     Pthread_detach(pthread_self());                                     //pear thread가 죽을 때 회수를 자동으로 해줌
     Free(vargp);                                                        //동적 메모리는 pear thread에서 free
-    //Signal(SIGPIPE, sigpipe_handler);      /**********/               //연결이 되지 않은 상태에서 통신 시 sigpie_handler 동작
+    //Signal(SIGPIPE, sigpipe_handler);                                 //연결이 되지 않은 상태에서 통신 시 sigpie_handler 동작
     proxy(connfd, &sockaddr);                                           //proxy 실행. 인자는 connfd랑 &sockaddr, sockaddr 는 로그파일 작성시 사용됨. 로그 파일 작성 전후로 pthread_mutex_lock, unlock 실행                                          
     Close(connfd);                                                      //pear thread에서 connfd close
     return NULL;
